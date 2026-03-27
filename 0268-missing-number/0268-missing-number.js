554fb28f94e606453791c1096eb448c86bca14d3
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-03-27 11:52:10 +0530

    Time: 25 ms (10.21%), Space: 59.7 MB (9.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let n = nums.length;
-    let sum = nums.reduce((sum,nums)=>nums+sum,0)
-    let sum2 = n*(n+1)/2
-    return sum2 - sum
+    nums.sort((a,b)=>a-b)
+    console.log(nums)
+    for(i=0; i<nums.length; i++){
+        if(nums[i]!==i)return i
+    }return nums.length
 };
