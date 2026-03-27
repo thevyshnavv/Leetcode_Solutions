9e7a5360c6ada2b95de7b4b1770026e9a026f2fd
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-03-27 12:49:47 +0530

    Time: 1 ms (73.42%), Space: 53.2 MB (96.84%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} title
+ * @return {string}
+ */
+var capitalizeTitle = function(title) {
+    return title
+    .split(" ")
+    .map((word)=>
+    {
+        if(word.length<=2){
+            return word.toLowerCase();
+        }else{
+            return word[0].toUpperCase()+word.slice(1).toLowerCase()
+        }
+    }
+    ).join(" ")
+};
