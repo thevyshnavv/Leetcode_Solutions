6d7ceeff65b674ae8a8df54f2aead139ce4f7ba0
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-03-27 12:00:53 +0530

    Time: 0 ms (100%), Space: 53.5 MB (74.36%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.js
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var alternateDigitSum = function(n) {
-    let str = n.toString()
     let sum = 0
+    let str = n.toString()
     for(let i=0; i<str.length; i++){
         let digit = Number(str[i])
-
         if(i%2===0){
             sum+=digit
-        }else{
+        }else {
             sum-=digit
         }
     }return sum
