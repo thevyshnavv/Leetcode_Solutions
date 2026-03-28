c3a45d95447c7bf8ef350b03c97459e25c50a3ba
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-03-28 16:46:41 +0530

    Time: 0 ms (100%), Space: 53.6 MB (74.78%) - LeetHub

---
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var getNoZeroIntegers = function (n) {
+    for (let a = 1; a < n; a++) {
+        let b = n - a
+        if (!a.toString().includes('0') && !b.toString().includes('0'))
+            return [a, b]
+    }
+};
