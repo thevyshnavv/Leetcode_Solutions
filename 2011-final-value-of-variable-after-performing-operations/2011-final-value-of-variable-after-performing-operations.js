01a89e9653a9a21726330ecc4af8b64718a8ebc9
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-04-09 11:33:47 +0530

    Time: 1 ms (44.47%), Space: 55.6 MB (33.61%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function (operations) {
+    let x = 0;
+    for (let op of operations) {
+        if (op.includes("++")) {
+            x++
+        } else {
+            x--
+        }
+    } return x
+};
