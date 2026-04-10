266f51f2514c2ec9856f282e18345fb90337a542
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-04-10 19:41:26 +0530

    Time: 0 ms (100%), Space: 56.3 MB (42.08%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -2,21 +2,11 @@
  * @param {number} num
  * @return {number}
  */
-/**
- * @param {number} num
- * @return {number}
- */
-var addDigits = function(num) {
-    while (num >= 10) {
-        let sum = 0;
-
-        while (num > 0) {
-            sum += num % 10;   
-            num = Math.floor(num / 10);
-        }
-
-        num = sum; 
-    }
-
-    return num;
+var addDigits = function (num) {
+    let sum = 0
+    while (num > 9) {
+        sum = num.toString().split("")
+        .reduce((sum, element) => sum + Number(element), 0)
+        num = sum
+    }return num
 };
