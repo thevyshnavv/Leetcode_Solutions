2ea1a0138aa94706296c7f2629e8737764378980
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-04-11 16:25:56 +0530

    Time: 0 ms (100%), Space: 53.8 MB (90.58%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -2,16 +2,15 @@
  * @param {number} n
  * @return {number[]}
  */
-var sumZero = function(n) {
-    result = []
-    if(n%2===1){
-        result.push(0);
+var sumZero = function (n) {
+    let result = []
+    if (n % 2 !== 0) {
+        result.push(0)
     }
-
-    let i=1
+    i = 1;
     while (result.length < n) {
-    result.push(i);
-    result.push(-i);
-    i++;
-  }return result 
+        result.push(-i)
+        result.push(i)
+        i++
+    } return result
 };
