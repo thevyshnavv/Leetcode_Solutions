234c7e4993cb3c09226d0b82fe4f00eb86495e31
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-04-25 09:20:54 +0530

    Time: 6 ms (77.6%), Space: 65.2 MB (85.8%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -2,18 +2,18 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDuplicates = function(nums) {
+function findDuplicates(nums) {
     let result = [];
 
     for (let i = 0; i < nums.length; i++) {
         let index = Math.abs(nums[i]) - 1;
 
         if (nums[index] < 0) {
-            result.push(Math.abs(nums[i]));
+            result.push(index + 1);
         } else {
             nums[index] = -nums[index];
         }
     }
 
     return result;
-};
+}
