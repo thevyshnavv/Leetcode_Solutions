9acfe50d94b5807eba429f995e2fadb04962308f
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-04-28 11:19:09 +0530

    Time: 1 ms (99.1%), Space: 68.8 MB (68.05%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function findMaxAverage(nums, k) {
+    let sum = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    let maxSum = sum;
+
+    for (let i = k; i < nums.length; i++) {
+        sum = sum + nums[i] - nums[i - k];
+        maxSum = Math.max(maxSum, sum);
+    }
+
+    return maxSum / k;
+}
