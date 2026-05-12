fa5fb6d089c39c9e8394c6b4e18dad6cb9ed1bba
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-05-12 15:43:38 +0530

    Time: 0 ms (100%), Space: 53.4 MB (62.71%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -2,7 +2,7 @@
  * @param {number} celsius
  * @return {number[]}
  */
-var convertTemperature = function (celsius) {
+var convertTemperature = function(celsius) {
     let kelvin = celsius + 273.15
     let fahrenheit = celsius * 1.80 + 32.00
     return [kelvin, fahrenheit]
