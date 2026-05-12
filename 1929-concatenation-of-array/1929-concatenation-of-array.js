6f307f10cbafc89574d728cb99ee4c8ca115717b
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-05-12 15:39:52 +0530

    Time: 0 ms (100%), Space: 56.3 MB (96.95%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    const n = nums.length
-    const ans = new Array(n*2)
-    for(let i = 0; i < n; i++){
-        ans[i] = nums[i]
-        ans[i+n] = nums[i]
-    }
-    return ans
+   return nums.concat(nums)
 };
