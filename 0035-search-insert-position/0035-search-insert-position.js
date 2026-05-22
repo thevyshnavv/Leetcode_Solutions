e2e45289d8d657ac7191a54d28a2996fa72b822c
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-05-22 11:32:33 +0530

    Time: 0 ms (100%), Space: 53.2 MB (87.71%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,16 +4,27 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length -1
-    while(left<=right){
-        let mid = Math.floor((left+right)/2)
-        if(nums[mid]===target){
-            return mid
-        }else if(nums[mid]<target){
-            left = mid+1
-        }else {
-            right = mid-1
+
+    let start = 0;
+    let end = nums.length - 1;
+    let mid = 0;
+
+    while (start <= end) {
+
+        mid = Math.floor(start + (end - start) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
         }
-    }return left 
+
+        else if (nums[mid] < target) {
+            start = mid + 1;
+        }
+
+        else {
+            end = mid - 1;
+        }
+    }
+
+    return target > nums[mid] ? mid + 1 : mid;
 };
