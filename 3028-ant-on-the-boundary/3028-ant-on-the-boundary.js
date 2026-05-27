0e660351f2d5e803d887e89801a39ce2aec6aa32
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-05-27 11:57:51 +0530

    Time: 47 ms (69.57%), Space: 53.8 MB (94.2%) - LeetHub

--- 3028-ant-on-the-boundary/3028-ant-on-the-boundary.js
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.js
@@ -2,15 +2,11 @@
  * @param {number[]} nums
  * @return {number}
  */
-var returnToBoundaryCount = function(nums) {
+var returnToBoundaryCount = function (nums) {
+    let boundary = 0
     let count = 0
-    let position = 0
-    for(let num of nums){
-        position+=num
-
-        if(position===0){
-            count++
-        }
-    }
-    return count
+    for (let i = 0; i < nums.length; i++) {
+        boundary +=nums[i]
+        if (boundary === 0) count++
+    }return count 
 };
