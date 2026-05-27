edf438b45b69d0705c01140c6627ea831ace2e47
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-05-27 11:27:07 +0530

    Time: 0 ms (100%), Space: 59 MB (7.2%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,18 +3,11 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    result = []
+    let result = []
     for(let i=1; i<=n; i++){
-        if(i%3===0&&i%5===0){
-            result.push("FizzBuzz")
-        }else if(i%3===0){
-            result.push("Fizz")
-        }
-        else if(i%5===0){
-            result.push("Buzz")
-        }else{
-            result.push(String(i))
-        }
-    }
-    return result
+        i%3===0&&i%5===0?result.push("FizzBuzz"):
+        i%3===0?result.push("Fizz"):
+        i%5===0?result.push("Buzz"):
+        result.push(String(i))
+    }return result
 };
