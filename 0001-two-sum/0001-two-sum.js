c14613abc650810f1c7c22bb2f46b46addadff7d
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-06-02 12:20:24 +0530

    Time: 33 ms (28.37%), Space: 54.3 MB (79.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0; i<nums.length; i++){
-        for(let j=0; j<nums.length; j++){
-            if(i===j)
-            break;
+    for(let i=0; i<nums.length-1; i++){
+        for(let j=i+1; j<nums.length; j++){
             if(nums[i]+nums[j]===target)
             return [i,j]
         }
     }
-}
-console.log(twoSum([2,7,11,15],9))
+};
