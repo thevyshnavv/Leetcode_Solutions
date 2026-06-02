dd11a5ebeac84648551cffb730ab89294665298a
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-06-02 12:07:09 +0530

    Time: 51 ms (10.56%), Space: 52.9 MB (89.44%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,9 +3,5 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    let arr = []
-    for(let i=0; i<address.length; i++){
-        if(address[i]===".")arr.push("[.]")
-        else arr.push(address[i])
-    }return arr.join("")
+    return address.split(".").join("[.]")
 };
