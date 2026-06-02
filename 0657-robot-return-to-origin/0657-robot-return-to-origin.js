afbc839fe453869772757594e35848fcb6f55ada
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-06-02 15:12:54 +0530

    Time: 6 ms (62.52%), Space: 55.4 MB (81%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.js
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -2,25 +2,14 @@
  * @param {string} moves
  * @return {boolean}
  */
-var judgeCircle = function (moves) {
-    let x = 0
-    let y = 0
-    for (let i = 0; i < moves.length; i++) {
-        switch (moves[i]) {
-            case "R":
-                x++;
-                continue;
-            case "L":
-                x--;
-                continue;
-            case "U":
-                y++;
-                continue;
-            case "D":
-                y--;
-                continue;
-            default:
-        }
+var judgeCircle = function(moves) {
+    let x=0
+    let y=0
+    for(let i=0; i<moves.length; i++){
+        if(moves[i]==="U")y++
+        if(moves[i]==="D")y--
+        if(moves[i]==="R")x++
+        if(moves[i]==="L")x--
     }
-    return x === 0 && y === 0
-};
+    return x===0&&y===0?true:false 
+}
