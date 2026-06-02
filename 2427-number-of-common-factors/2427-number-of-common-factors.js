d10213ec890277cfb9d7c067750294a862396568
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-06-02 11:42:40 +0530

    Time: 1 ms (39.86%), Space: 55.1 MB (14.19%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.js
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -4,11 +4,9 @@
  * @return {number}
  */
 var commonFactors = function(a, b) {
-    let count = 0;
-    for(let i=1; i<=Math.min(a,b); i++){
-        if(a%i===0&&b%i===0){
-            count++
-        }
-    }
-    return count
+    let large = a<b?b:a
+    let count = 0
+    for(let i=1; i<=large; i++){
+        if(a%i===0&&b%i===0)count++
+    }return count 
 };
