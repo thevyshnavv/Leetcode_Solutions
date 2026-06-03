2dab5a790d86c0059c7e79fb9ec0fc0023983c98
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-06-03 10:16:15 +0530

    Time: 9 ms (38.45%), Space: 65.1 MB (25.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,14 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x<0)
-    return false 
-    const y=Number(String(x).split("").reverse().join(""))
-    if(x === y){
-        return true
-    }else{
-        return false
-    }
+    if(x>=0&&x.toString().split("").reverse().join("")===x.toString())return true
+    else return false 
 };
-
-console.log(isPalindrome(121))
