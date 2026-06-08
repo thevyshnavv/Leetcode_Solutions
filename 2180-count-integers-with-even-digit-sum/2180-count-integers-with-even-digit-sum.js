b9f4d349406829668c2e882deac159aa96d0ef77
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-06-08 22:24:43 +0530

    Time: 0 ms (100%), Space: 55.7 MB (37.61%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -2,13 +2,14 @@
  * @param {number} num
  * @return {number}
  */
-var countEven = function(num) {
-    let count = 0
-    for(let i=1; i<=num; i++){
-        sum = 0
-        for(let n of i.toString())
-        sum+=Number(n)
-        if(sum%2 === 0)count++
+function countEven(num) {
+    let sum = 0;
+    let n = num;
+
+    while (n > 0) {
+        sum += n % 10;
+        n = Math.floor(n / 10);
     }
-    return count
-};
+
+    return sum % 2 === 0 ? Math.floor(num / 2) : Math.floor((num - 1) / 2);
+}
