253b0332adeebf7797853fd473f296591fdca8dd
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-06-16 14:28:45 +0530

    Time: 8 ms (80.41%), Space: 60.2 MB (92.63%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+
+    const onlyInNums1 = [...set1].filter(num => !set2.has(num));
+    const onlyInNums2 = [...set2].filter(num => !set1.has(num));
+
+    return [onlyInNums1, onlyInNums2];
+};
