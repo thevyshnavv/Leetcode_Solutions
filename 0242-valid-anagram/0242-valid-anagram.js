a628b5499a92b36b29616d79f3921bd8942393c7
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-06-22 12:35:52 +0530

    Time: 12 ms (70.89%), Space: 59.6 MB (25.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,13 +4,18 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length!==t.length)return false
-    const count  = {}
-    for(let char of s){
-        count[char] = (count[char]||0)+1
+    if (s.length !== t.length) return false;
+
+    const count = {};
+
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
     }
-    for(let char of t){
-        if(!count[char])return false
-        count[char]--
-    }return true 
+
+    for (let char of t) {
+        if (!count[char]) return false;
+        count[char]--;
+    }
+
+    return true;
 };
