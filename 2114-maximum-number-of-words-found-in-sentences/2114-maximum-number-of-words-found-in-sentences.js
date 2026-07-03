5d068cbac539d4cb19c637980836717184100ca3
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-07-03 11:17:02 +0530

    Time: 2 ms (76.34%), Space: 56.1 MB (61.51%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -2,22 +2,13 @@
  * @param {string[]} sentences
  * @return {number}
  */
-function mostWordsFound(sentences) {
-  let max = 0;
-
-  for (let i = 0; i < sentences.length; i++) {
-    let count = 1; // at least one word
-
-    for (let j = 0; j < sentences[i].length; j++) {
-      if (sentences[i][j] === " ") {
-        count++;
-      }
-    }
-
-    if (count > max) {
-      max = count;
-    }
-  }
-
-  return max;
-}
+var mostWordsFound = function(sentences) {
+    let count 
+    let max = 0
+    for(let i=0; i<sentences.length; i++){
+        count = 0
+        for(let j=0; j<sentences[i].length; j++){
+            if(sentences[i][j]===" ")count++
+        }if(count>max)max=count
+    }return max+1
+};
