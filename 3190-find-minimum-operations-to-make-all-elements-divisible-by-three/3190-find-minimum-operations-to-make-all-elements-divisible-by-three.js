32881b75159ab84d6da5e57b40f64d70d9a2b40b
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-07-03 16:59:14 +0530

    Time: 1 ms (42.28%), Space: 55.5 MB (62.6%) - LeetHub

---
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+    let op = 0
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]%3!==0)op++
+    }return op
+};
