a645649941f47a498b89131a2ccce9483ec568f9
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-07-11 12:06:33 +0530

    Time: 0 ms (100%), Space: 55.4 MB (9.48%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.js
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -4,9 +4,10 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
+    let jewelsSet = new Set(jewels)
     let count = 0
     for(let stone of stones){
-        if(jewels.includes(stone)){
+        if(jewelsSet.has(stone)){
             count++
         }
     }return count
