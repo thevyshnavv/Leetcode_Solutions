02fcdfe72a7a38e3347ef7f264d4c545ba3242a9
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-08-01 22:43:42 +0530

    Time: 8 ms (54.8%), Space: 57.6 MB (61.16%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    let result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
+                count++;
+            }
+        }
+
+        result.push(count);
+    }
+
+    return result;
+};
+
+// Example
+let nums = [8, 1, 2, 2, 3];
+
+console.log(smallerNumbersThanCurrent(nums));
+// Output: [4, 0, 1, 1, 3]
