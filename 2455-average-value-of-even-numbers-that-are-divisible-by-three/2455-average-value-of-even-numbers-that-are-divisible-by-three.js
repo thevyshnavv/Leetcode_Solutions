1a0fa2479b2d9ce1b9554de330ff789a810c2069
VYSHNAV T <165544373+thevyshnavv@users.noreply.github.com>
2026-09-23 11:32:34 +0530

    Time: 2 ms (41.38%), Space: 54.2 MB (74.71%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.js
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -1,14 +1,17 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var averageValue = function (nums) {
-    let sum = 0
-    let count = 0
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] % 2 === 0 && nums[i] % 3 === 0) {
-            sum += nums[i] 
-            count++
+var averageValue = function(nums) {
+    let sum = 0;
+    let count = 0;
+
+    for (let num of nums) {
+        if (num % 2 === 0 && num % 3 === 0) {
+            sum += num;
+            count++;
         }
-    } return count === 0?0:Math.floor(sum/count)
-}
+    }
+
+    if (count === 0) {
+        return 0;
+    }
+
+    return Math.floor(sum / count);
+};
